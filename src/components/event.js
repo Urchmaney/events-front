2906d20a1f6fa8f7d94ef4898d59fe4338065735
Urchmaney <kingsobino@gmail.com>
2019-12-09 22:51:26 +0000

    Project Setup: Create react app, add stickler config files, Remove unnecessary react files.

---
+++ src/components/event.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Indicator from './indicator';
+import '../styles/style.css';
+
+const Event = (props) => {
+  const { location } = props;
+  return (
+    <div className="event">
+      <Indicator />
+      <p>9.00 to 10</p>
+      <div className="info-container">
+        <div>
+          <h2>
+          Opening Spech
+          </h2>
+          <p>
+          host
+            <span>{location}</span>
+          </p>
+        </div>
+        <span className="interested">
+        Interested
+        </span>
+      </div>
+    </div>
+
+  );
+};
+
+export default Event;
---
+++ src/components/indicator.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../styles/style.css';
+
+const Indicator = () => (
+  <span className="indicator" />
+);
+
+export default Indicator;
