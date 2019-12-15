860df8790073f0c7fd37f92cdd97704f0f76b35f
Urchmaney <kingsobino@gmail.com>
2019-12-15 02:15:48 +0000

    Add fontawesome library for icon i will be using.

---
+++ src/fontawesome.js
@@ -0,0 +1,28 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faBell,
+  faBars,
+  faArrowLeft,
+  faCalendarAlt,
+  faUsers,
+  faArchway,
+  faCommentAlt,
+  faHome,
+  faCalendarCheck,
+  faTimes,
+  faPlus,
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+  faBell,
+  faBars,
+  faArrowLeft,
+  faCalendarAlt,
+  faUsers,
+  faArchway,
+  faCommentAlt,
+  faHome,
+  faCalendarCheck,
+  faTimes,
+  faPlus,
+);
