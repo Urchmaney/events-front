9a0bf4e0563a291c48e82cdca2fba89172d8a2c6
Urchmaney <kingsobino@gmail.com>
2019-12-15 02:22:26 +0000

    Implement menu toggle in home. and pass prop to header for onclick for open menu.

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,33 @@
 import React from 'react';
-import '../styles/style.css';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import '../styles/style.scss';
 
-const Header = () => (
-  <div className="header"> Agenda </div>
-);
+const Header = (props) => {
+  const { fontType, title, notifyIcon, onClick } = props;
+  console.log(onClick);
+  return (
+    <div className="header">
+      <span className="icon-span-head" onClick={onClick} role="button" onKeyUp={onClick} tabIndex={0}>
+        <FontAwesomeIcon icon={fontType} />
+      </span>
+      {title}
+      <span className="icon-span">
+        {notifyIcon && <FontAwesomeIcon icon="bell" />}
+      </span>
+    </div>
+  );
+};
+
+Header.propTypes = {
+  fontType: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  notifyIcon: PropTypes.bool,
+};
+
+Header.defaultProps = {
+  notifyIcon: false,
+};
 
 export default Header;
--- src/components/home.js
+++ src/components/home.js
@@ -2,26 +2,69 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import HomeTimeDisplay from './home-time-display';
+import EventFeatureList from './event-feature-list';
+import HomeMenu from './home-meun';
 import EventList from './event-list';
+import EventDescription from './event-description';
 import Header from './header';
 import Footer from './footer';
-import '../styles/style.css';
+import '../styles/style.scss';
 
-const Home = (props) => {
-  const { isLoggedIn } = props;
-  if (!isLoggedIn) {
-    return (<Redirect to="/login" />);
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuOn: false,
+      shift: 0,
+    };
+    this.showMenu = this.showMenu.bind(this);
+    this.HideMenu = this.HideMenu.bind(this);
   }
-  return (
-    <div className="container">
-      <Header />
-      <div className="main">
+
+  showMenu() {
+    this.setState({
+      menuOn: true,
+      shift: 100,
+    });
+  }
+
+  HideMenu() {
+    this.setState({
+      menuOn: false,
+      shift: 0,
+    });
+  }
+
+  render() {
+    const { isLoggedIn } = this.props;
+    const { menuOn, shift } = this.state;
+    const { HideMenu, showMenu } = this;
+    const shiftStyle = {
+      left: shift,
+    };
+    if (!isLoggedIn) {
+      return (<Redirect to="/login" />);
+    }
+    return (
+      <div className="container">
+        <HomeMenu onClick={HideMenu} show={menuOn} />
+        <div className="content-main" style={shiftStyle}>
+          <EventDescription />
+          <Header fontType="bars" title="Home" onClick={showMenu} notifyIcon />
+          <HomeTimeDisplay days="2" hr="4" minutes="40" />
+          <EventFeatureList />
+        </div>
+
+
+        {/* <div className="main">
         <EventList />
         <Footer />
+      </div> */}
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 const mapStateToProps = (state) => ({
   isLoggedIn: state.loggedIn,
