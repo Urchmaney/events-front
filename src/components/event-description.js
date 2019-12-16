0efd3175f62f32a69ca1a6d96b47486ab42f0c4f
Urchmaney <kingsobino@gmail.com>
2019-12-16 19:41:59 +0000

    Connect event description to api

--- src/components/event-description.js
+++ src/components/event-description.js
@@ -1,50 +1,119 @@
+/* eslint-disable react/forbid-prop-types */
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { get } from '../services/call';
 import Header from './header';
+import Organizers from './organizer';
+import { organizersUrl } from '../constants';
 
+class EventDescription extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { organizers: {} };
+    this.returnHome = this.returnHome.bind(this);
+  }
 
-const EventDescription = () => (
-  <div className="event-desc">
-    <Header fontType="arrow-left" title="Description" onClick={() => {}} />
-    <div className="event-main">
-      <div className="date-location">
-        <div className="wrapper">
-          <div className="img-container">
-            <FontAwesomeIcon icon="home" />
+  componentDidMount() {
+    const { event, token } = this.props;
+    const getResult = get(organizersUrl(event.id), token);
+    getResult.then((result) => {
+      if (!result.error) {
+        this.setState({ organizers: result });
+      }
+    });
+  }
+
+  returnHome() {
+    const { history } = this.props;
+    history.push('/');
+  }
+
+  render() {
+    const { organizers } = this.state;
+    const { history, event } = this.props;
+    const { returnHome } = this;
+    return (
+      <div className="event-desc">
+        <Header fontType="arrow-left" title="Description" onClick={returnHome} />
+        <div className="event-main">
+          <div className="date-location">
+            <div className="wrapper">
+              <div className="img-container">
+                <FontAwesomeIcon icon="home" />
+              </div>
+              <div className="text-container">
+                <p className="title">
+                Date & Time
+                </p>
+                <p className="date-time">
+                  {new Date(event.created_at).toString().split(' ', 4).join(' ')}
+                </p>
+              </div>
+            </div>
+            <div className="wrapper">
+              <div className="img-container">
+                <FontAwesomeIcon icon="home" />
+              </div>
+              <div className="text-container">
+                <p className="title">
+                location
+                </p>
+                <p className="date-time">
+                  {event.location}
+                </p>
+              </div>
+            </div>
           </div>
-          <div className="text-container">
-            <p className="title">
-              Date & Time
-            </p>
-            <p className="date-time">
-              october 15,2018 09:00am - 10:00am
-            </p>
+          <div />
+          <div className="add-schedule">
+          Add to Schedule
+            <span>
+              <FontAwesomeIcon icon="plus" />
+            </span>
           </div>
         </div>
-        <div className="wrapper">
-          <div className="img-container">
-            <FontAwesomeIcon icon="home" />
-          </div>
-          <div className="text-container">
-            <p className="title">
-              Date & Time
-            </p>
-            <p className="date-time">
-              october 15,2018 09:00am - 10:00am
-            </p>
+        <div className="desc-main">
+          <h3 className="desc-title">Description</h3>
+          <p className="desc-body">
+            {event.description}
+          </p>
+        </div>
+        <div>
+          {
+             Object.keys(organizers).map((key) => {
+               return (<Organizers key={key} title={key} people={organizers[key]} />);
+             })
+          }
+        </div>
+        <div>
+          <div
+            className="add-schedule"
+            onClick={() => { history.push('/event/discussion'); }}
+            onKeyDown={() => {}}
+            role="presentation"
+          >
+          Join Conversation
+            <span>
+              <FontAwesomeIcon icon="comment" />
+            </span>
           </div>
         </div>
       </div>
-      <div />
-      <div className="add-schedule">
-        Add to Schedule
-        <span>
-          <FontAwesomeIcon icon="plus" />
-        </span>
-      </div>
-    </div>
+    );
+  }
+}
 
-  </div>
-);
+const mapStateToProps = (state) => ({
+  event: state.event,
+  token: state.token,
+});
 
-export default EventDescription;
+EventDescription.propTypes = {
+  history: PropTypes.object.isRequired,
+  event: PropTypes.object.isRequired,
+  token: PropTypes.object.isRequired,
+};
+
+export default connect(mapStateToProps)(EventDescription);
