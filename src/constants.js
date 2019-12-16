da20e64c9638dfcbc124c8f9d85274612dc4c88a
Urchmaney <kingsobino@gmail.com>
2019-12-16 19:39:10 +0000

    Add millisecond constants and comment and organizers url

--- src/constants.js
+++ src/constants.js
@@ -1,13 +1,27 @@
 const LOGIN = 'LOGIN';
 const LOGOUT = 'LOGOUT';
 const ADD_TOKEN = 'ADD TOKEN';
+const CHANGE_EVENT = 'CHANGE_EVENT';
+const daysInMillSec = 24 * 60 * 60000;
+const hoursInMilliSec = 60 * 60000;
+const minutesInMilliSec = 60000;
 const registerUrl = 'http://localhost:3000/api/v1/register';
 const loginUrl = 'http://localhost:3000/api/v1/login';
+const allEventUrl = 'http://localhost:3000/api/v1/events/all';
+const organizersUrl = id => `http://localhost:3000/api/v1/events/${id}/organizers`;
+const commentUrl = id => `http://localhost:3000/api/v1/events/${id}/comments`;
 
 export {
   LOGIN,
   LOGOUT,
   ADD_TOKEN,
+  CHANGE_EVENT,
   registerUrl,
   loginUrl,
+  allEventUrl,
+  daysInMillSec,
+  hoursInMilliSec,
+  minutesInMilliSec,
+  organizersUrl,
+  commentUrl,
 };
